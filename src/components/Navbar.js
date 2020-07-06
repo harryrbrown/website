23e9d23fa3198bb7ba2780d858539b98dc8e34dd
harryrbrown <harry.brown26@gmail.com>
2020-07-06 12:42:49 +0100

    Mobile optimisations
    Signed-off-by: harryrbrown <harry.brown26@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
 
+import Hamburger from './../img/hamburger.svg';
+import Social from './../img/social.svg';
+
 export default class Navbar extends React.Component{
   render(){
-    return (
-      <div className="nav">
-        <NavLink activeClassName="is-active" to="/" exact>HOME </NavLink>
-        <NavLink activeClassName="is-active" to="/about">ABOUT </NavLink>
-        <NavLink activeClassName="is-active" to="/work">WORK </NavLink>
-        <NavLink activeClassName="is-active" to="/contact">CONTACT </NavLink>
-      </div>
-    );
+      return (
+        <div>
+          <div className="nav">
+            <NavLink activeClassName="is-active" to="/" exact>HOME </NavLink>
+            <NavLink activeClassName="is-active" to="/about">ABOUT </NavLink>
+            <NavLink activeClassName="is-active" to="/work">WORK </NavLink>
+            <NavLink activeClassName="is-active" to="/contact">CONTACT </NavLink>
+          </div>
+
+          <div className="mobileNav">
+            <img className="hamburger" src={Hamburger} /><br/><br/>
+            <img className="hamburger" src={Social} />
+          </div>
+        </div>
+      );
   }
 }
