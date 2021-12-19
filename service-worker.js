1037309c671c2f9b5dadf3647619c749a7575af1
harryrbrown <harry.brown26@gmail.com>
2021-12-19 17:23:28 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b5136064c5d7c21eb8afa80ac2f32e6.js"
+  "/precache-manifest.38c45246261112a29ddb2fc7cff4be7a.js"
 );
 
 self.addEventListener('message', (event) => {
