8d1c0a9fa17b166b19bbeb15679339f0b7aa8fdc
harryrbrown <harry.brown26@gmail.com>
2021-12-21 23:09:13 +0000

    Dark mode formatting changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
 
+import linkedin from './../img/052-linkedin.svg';
+import github from './../img/032-github.svg';
+import twitter from './../img/096-twitter.svg';
+import spotify from './../img/082-spotify.svg';
+import _switch from './../img/switch.svg';
 
 export default class Contact extends React.Component{
   render(){
@@ -15,6 +20,13 @@ export default class Contact extends React.Component{
             </div>
             <div>
                 <p>Please get in touch with me via the social media buttons 🙂</p>
+            </div><br/><br/>
+            <div>
+                <a href="https://www.linkedin.com/in/hryrbrn"><img className="socialIcon" src={linkedin} /></a>
+                <a href="https://www.github.com/harryrbrown"><img className="socialIcon" src={github} /></a>
+                <a href="https://www.twitter.com/hryrbrn"><img className="socialIcon" src={twitter} /></a>
+                <a href="https://open.spotify.com/user/21ttlagltvoefrfidydhporzy?si=lN9Qq27STW6V_jDSY_9Jkg"><img className="socialIcon" src={spotify}/></a>
+                <Link to="/switch"><img className="socialIcon" src={_switch} /></Link><br/><br/>
             </div>
         </div>
     );
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -15,6 +15,8 @@ import chevillagehall from './../img/che.jpeg';
 import pierview from './../img/pierview_border.png';
 import grundstein from './../img/grundstein.jpeg';
 import fallen from './../img/fallen.png';
+import screenprint from './../img/screenprint.png';
+import fractals from './../img/fractals.png';
 
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './home.css';
@@ -41,11 +43,17 @@ const FilledCard = ({ img, title, description, onclick, cardWidth }) => (
 );
 
 export default class About extends React.Component{
-    state = {visible: false};
+    state = {
+        visible: false,
+        drawer_title: "",
+        drawer_contents: <></>
+    };
 
-    showDrawer = () => {
+    showDrawer = (drawer_title, drawer_contents) => {
         this.setState({
             visible: true,
+            drawer_title: drawer_title,
+            drawer_contents: drawer_contents
         });
     };
     
@@ -56,8 +64,6 @@ export default class About extends React.Component{
     };
 
     render(){
-        const { visible } = this.state;
-
         var column_span = 8;
         var card_width = 400;
         if (window.innerWidth < 1024) {
@@ -77,75 +83,142 @@ export default class About extends React.Component{
                     
                 <Divider orientation="left"><h1 style={{color: "#E01A4F"}}>Web Design</h1></Divider><br></br>
                 <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={newcroft} title="Haverhill Community Sports Association" cardWidth={card_width}
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={newcroft} 
+                            title="Haverhill Community Sports Association" 
+                            // cardWidth={card_width}
                             description="Since 2015 I have designed and maintained the website for The New Croft, the sports and leisure complex operated by the Haverhill
                             Community Sports Association in Haverhill, Suffolk. In 2020 the website was extended to include an online bar, allowing customers to order
-                            food and drink directly to their seat, to facilitate the site's reopening following the coronavirus pandemic."></FilledCard><br></br>
+                            food and drink directly to their seat, to facilitate the site's reopening following the coronavirus pandemic.">
+                        </FilledCard><br></br>
                     </Col>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={newtondavid} title="Newton David Ltd" cardWidth={card_width}
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={newtondavid} 
+                            title="Newton David Ltd" 
+                            // cardWidth={card_width}
                             description="Newton David is a health and safety consultancy based in Haverhill, Suffolk and operating across the UK. The company provides a
-                            wide range of services, including delivering IOSH training, and wanted a small and lightweight website to promote their business. "></FilledCard>
-                            <br></br>
+                            wide range of services, including delivering IOSH training, and wanted a small and lightweight website to promote their business. ">
+                        </FilledCard><br></br>
                     </Col>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={londonbusinessschool} title="London Business School Healthcare Conference" cardWidth={card_width}
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={londonbusinessschool} 
+                            title="London Business School Healthcare Conference" 
+                            // cardWidth={card_width}
                             description="I was commissioned by London Business School's Healthcare Club to create a website for their 10th and 11th annual conferences in 
-                            2019 and 2020."></FilledCard>
+                            2019 and 2020.">
+                        </FilledCard>
                     </Col>
                 </Row><br></br><br></br>
                 <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={chevillagehall} title="Pierview Holidays" cardWidth={card_width}
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={chevillagehall} 
+                            title="Cornish Hall End Village Hall" 
+                            // cardWidth={card_width}
                             description="I designed a new website for the Cornish Hall End Village Hall, after it was refurbished and reopened in 2020. I then trained some
-                            of the Hall's staff on updating the site and adding new content."></FilledCard><br></br>
+                            of the Hall's staff on updating the site and adding new content.">
+                        </FilledCard><br></br>
                     </Col>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={pierview} title="Pierview Holidays" cardWidth={card_width}
-                            description="In 2020 I helped to build a lettings website for a holiday property in North Wales."></FilledCard><br></br>
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={pierview} 
+                            title="Pierview Holidays" 
+                            // cardWidth={card_width}
+                            description="In 2020 I helped to build a lettings website for a holiday property in North Wales.">
+                        </FilledCard><br></br>
                     </Col>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={grundstein} title="Grundstein Global" cardWidth={card_width}
-                            description="I developed the website for Grundstein Global using Spring Boot."></FilledCard><br></br>
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={grundstein} 
+                            title="Grundstein Global" 
+                            // cardWidth={card_width}
+                            description="I developed the website for Grundstein Global using Spring Boot.">
+                        </FilledCard><br></br>
                     </Col>
                 </Row>
                 <br></br><br></br>
 
                 <Divider orientation="left"><h1 style={{color: "#E01A4F"}}>University</h1></Divider><br></br>
                 <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={sparsematrix} title="An aggregated sparse matrix factorisation model for market trading" cardWidth={card_width}
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={sparsematrix} 
+                            title="An aggregated sparse matrix factorisation model for market trading" 
+                            // cardWidth={card_width}
                             description="For my final year project, I developed a sparce matrix factorisation model to analyse the relationship between public sentiment on Twitter,
-                            Reuters news articles, and stock market prices. My work was selected among the Department of Computing's 2019/20 Distinguished Projects."></FilledCard><br></br>
+                            Reuters news articles, and stock market prices. My work was selected among the Department of Computing's 2019/20 Distinguished Projects.">
+                        </FilledCard><br></br>
                     </Col>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={sentimentanalysis} title="Aspect-based sentiment analysis for investment" cardWidth={card_width}
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={sentimentanalysis} 
+                            title="Aspect-based sentiment analysis for investment" 
+                            // cardWidth={card_width}
                             description="Working with Goldman Sachs, we investigated streamlining the research process of a portfolio manager by using aspect-based sentiment
-                            analysis to automatically parse news articles and documents."></FilledCard><br></br>
+                            analysis to automatically parse news articles and documents.">
+                        </FilledCard><br></br>
                     </Col>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={prosopagnosiapp} title="ProsopagnosiApp" cardWidth={card_width}
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={prosopagnosiapp} 
+                            title="ProsopagnosiApp" 
+                            // cardWidth={card_width}
                             description="Second year university project in collaboration with Microsoft and Brunel University. A tool was developed to provide an initial online
                             screening for prosopagnosia. Using Google Glass, we then developed a prototype software to assist people with prosopagnosia in recognising familiar
-                            faces. My personal highlight of university! 😃"></FilledCard>
+                            faces. My personal highlight of university! 😃">
+                        </FilledCard>
                     </Col>
                 </Row>
                 <br></br><br></br>
                 <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img="https://raw.githubusercontent.com/harryrbrown/harryrbrown.github.io/f6c2771eccfeab4a6fb589ecb8ec4e2ef366b4dd/img/arm11pim.png" title="PIM" 
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img="https://raw.githubusercontent.com/harryrbrown/harryrbrown.github.io/f6c2771eccfeab4a6fb589ecb8ec4e2ef366b4dd/img/arm11pim.png" 
+                            title="PIM" 
+                            // cardWidth={card_width}
                             description="Award-winning first year university project, involving writing an ARM11 emulator and assembler in C, followed by recreating Pong from scratch to 
-                            run on a Raspberry Pi." cardWidth={card_width}></FilledCard><br></br>
+                            run on a Raspberry Pi.">
+                        </FilledCard><br></br>
                     </Col>
                 </Row>
 
                 <br></br><br></br>
                 <Divider orientation="left"><h1 style={{color: "#E01A4F"}}>Apps</h1></Divider><br></br>
                 <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                    <Col className="gutter-row" span={column_span}>
-                        <FilledCard img={fallen} title="Fallen" onclick={this.showDrawer} cardWidth={card_width}
-                            description="Mobile game released for Android in 2014."></FilledCard><br></br>
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={fallen} 
+                            title="Fallen" 
+                            onclick={() => this.showDrawer("Fallen", <><p>Written in Unity, Fallen is a game where you fall while trying to avoid hitting the sides of an 
+                                endlessly-generating pipe. It later incorporated Google Play's achievements and leaderboard functionality for competitive play.</p>
+                                <p><b>Link: <a href="https://play.google.com/store/apps/details?id=com.Suune.Faller">https://play.google.com/store/apps/details?id=com.Suune.Faller</a></b></p></>)} 
+                            // cardWidth={card_width}
+                            description="Mobile game released for Android in 2014.">
+                        </FilledCard><br></br>
+                    </Col>
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={screenprint} 
+                            title="ScreenPrint" 
+                            onclick={() => this.showDrawer("ScreenPrint", <><p>ScreenPrint was my first useful project in VB.NET, borne out of frustration at Windows 7's lack of 
+                                keyboard-friendly screenshotting options. ScreenPrint sits in the application tray and can take full-screen, active window or region-based screenshots 
+                                at the input of a keyboard shortcut of the user's choice (on Mac, the equivalent seems to be ⇧-⌘-Ctrl-3/4). I still use it to this day, mainly out of habit.
+                                </p><p><b>Link: <a href="https://drive.google.com/file/d/1TxczzTD0Uv5DUXE-TalJrFosPvG7qmIK/view?usp=sharing">Download</a></b></p></>)} 
+                            // cardWidth={card_width}
+                            description="Improved screen capturing for Windows. Written in .NET in 2015.">
+                        </FilledCard><br></br>
+                    </Col>
+                    <Col xs={24} md={16} xl={8}>
+                        <FilledCard 
+                            img={fractals} 
+                            title="OpenCL Fractals" 
+                            onclick={() => this.showDrawer("OpenCL Fractals", <>Test</>)} 
+                            // cardWidth={card_width}
+                            description="Python fractal generator, leveraging OpenCL to improve rendering speeds.">
+                        </FilledCard><br></br>
                     </Col>
                 </Row>
 
@@ -154,17 +227,14 @@ export default class About extends React.Component{
                 </div>
 
                 <Drawer
-                    title="An aggregated sparse matrix factorisation model for market trading"
+                    title={this.state.drawer_title}
                     placement="left"
-                    width={640}
-                    closable={false}
+                    // width={640}
                     onClose={this.onClose}
-                    visible={visible}
+                    visible={this.state.visible}
                     key="left"
                     >
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
+                    {this.state.drawer_contents}
                 </Drawer>
             </div>
             </div>
